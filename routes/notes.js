2be098e342309be1ecfc9e8372d32d7c5f926885
Deven <devenkapadia1@gmail.com>
2024-07-06 21:49:32 +0530

    desc full  abr

--- routes/notes.js
+++ routes/notes.js
@@ -21,18 +21,18 @@ router.post('/addnote', fetchuser, [
     body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),], async (req, res) => {
         try {
             const { title, description, tag } = req.body;
-
             // If there are errors, return Bad request and the errors
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
                 return res.status(400).json({ errors: errors.array() });
-            }
-            const note = new Note({
-                title, description, tag, user: req.user.id
-            })
-            const savedNote = await note.save()
+            } else {
+                const note = new Note({
+                    title, description, tag, user: req.user.id
+                })
+                const savedNote = await note.save()
 
-            res.json(savedNote)
+                res.json(savedNote)
+            }
 
         } catch (error) {
             console.error(error.message);
