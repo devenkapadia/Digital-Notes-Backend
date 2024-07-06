259a6bbb3d6be5c303447ce5bbaa6d3bea5ea104
Deven <devenkapadia1@gmail.com>
2024-07-06 23:47:58 +0530

    get route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express')
-var cors = require('cors') 
+var cors = require('cors')
 
 connectToMongo();
 const app = express()
@@ -10,6 +10,12 @@ app.use(cors())
 app.use(express.json())
 
 // Available Routes
+
+// Simple route to check if the app is listening
+app.get('/', (req, res) => {
+  res.send('App is listening and running!');
+});
+
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
